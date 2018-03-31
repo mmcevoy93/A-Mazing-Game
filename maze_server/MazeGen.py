5bf78e8f2fe6af3e404cb0cdbf6066e3acec269e
Nicholas Serrano <34073804+nickroxcks@users.noreply.github.com>
2018-03-31 17:53:23 -0600

    MazeGenerationCode

---
+++ maze_server/MazeGen.py
@@ -0,0 +1,14 @@
+#class Arrays:
+#	def __init__(self,x,y):
+unvisited = []
+for i in range(3):
+	unvisited.append(1)
+	unvisited[i] = []
+	for j in range(3):
+		unvisited[i].append("B")
+
+
+
+print(unvisited)
+#test = [ [0,1], [2,3]]
+#print(test[1][0])
